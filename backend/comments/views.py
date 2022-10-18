ebc7486c64e5c5ec0d0251708ad15c3f8e11b67b
gregreust <greg.reust@gmail.com>
2022-10-18 15:43:11 -0500

    Created endpoint for getting all comments associated with a video id. Needs testing

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Comments
+from .serializers import CommentsSerializer
+from django.shortcuts import get_list_or_404
 
-# Create your views here.
+# This endpoint gets the video_id in the URL
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_video_comments(request, video_id):
+
+    comments = get_list_or_404(Comments, video_id = video_id)
+    serializer = CommentsSerializer(comments)
+    return Response(serializer.data)
+
